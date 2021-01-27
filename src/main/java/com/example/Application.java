5fee55b7952af1806e18323dc03b20168fa05064
孟源运 <my@guiyunweb.com>
2021-01-27 21:24:24 +0800

    1.0.0

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author example
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class Application
+{
+    public static void main(String[] args)
+    {
+         System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(Application.class, args);
+        System.out.println("Start successfully!!!");
+    }
+}
